d6339d37a49e33ff523fb74066db6a254f663020
tianyu <mail@jxytdl.com>
2019-04-25 16:57:47 +0800

    #更新首页

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,32 +1,38 @@
 <template>
-  <div class="dashboard-container">
-    <div class="dashboard-text">name:{{ name }}</div>
-    <div class="dashboard-text">roles:<span v-for="role in roles" :key="role">{{ role }}</span></div>
+  <div class="container">
+    <div class="block">
+      <el-timeline>
+        <el-timeline-item timestamp="2019/4/24" placement="top">
+          <el-card>
+            <h4>更新 上传2小节到每周CSS2019专栏</h4>
+            <p>Xiao-Cuir 提交于 2018/4/24 21:35</p>
+          </el-card>
+        </el-timeline-item>
+        <el-timeline-item timestamp="2019/4/13" placement="top">
+          <el-card>
+            <h4>更新 修改框架路由及webpack部分配置</h4>
+            <p>Xiao-Cuir 提交于 2019/4/13 22:03</p>
+          </el-card>
+        </el-timeline-item>
+        <el-timeline-item timestamp="2019/4/1" placement="top">
+          <el-card>
+            <h4>创建 网站基础模板创建</h4>
+            <p>Xiao-Cuir 提交于 2019/4/1 22:15</p>
+          </el-card>
+        </el-timeline-item>
+      </el-timeline>
+    </div>
   </div>
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
 
 export default {
-  name: 'Dashboard',
-  computed: {
-    ...mapGetters([
-      'name',
-      'roles'
-    ])
-  }
 }
 </script>
 
 <style rel="stylesheet/scss" lang="scss" scoped>
-.dashboard {
-  &-container {
-    margin: 30px;
-  }
-  &-text {
-    font-size: 30px;
-    line-height: 46px;
-  }
+.container {
+  padding: 20px 40px 20px 20px;
 }
 </style>
