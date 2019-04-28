84f7b159a7779db25912cd8375c8ba0933b4c931
tianyu <mail@jxytdl.com>
2019-04-28 14:08:30 +0800

    #更新首页

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,38 +1,21 @@
 <template>
   <div class="container">
-    <div class="block">
-      <el-timeline>
-        <el-timeline-item timestamp="2019/4/24" placement="top">
-          <el-card>
-            <h4>更新 上传2小节到每周CSS2019专栏</h4>
-            <p>Xiao-Cuir 提交于 2018/4/24 21:35</p>
-          </el-card>
-        </el-timeline-item>
-        <el-timeline-item timestamp="2019/4/13" placement="top">
-          <el-card>
-            <h4>更新 修改框架路由及webpack部分配置</h4>
-            <p>Xiao-Cuir 提交于 2019/4/13 22:03</p>
-          </el-card>
-        </el-timeline-item>
-        <el-timeline-item timestamp="2019/4/1" placement="top">
-          <el-card>
-            <h4>创建 网站基础模板创建</h4>
-            <p>Xiao-Cuir 提交于 2019/4/1 22:15</p>
-          </el-card>
-        </el-timeline-item>
-      </el-timeline>
-    </div>
+    <Timeline />
   </div>
 </template>
 
 <script>
+import Timeline from '@/views/everycss/2019/timeline'
 
 export default {
+  components: {
+    Timeline
+  }
 }
 </script>
 
 <style rel="stylesheet/scss" lang="scss" scoped>
 .container {
-  padding: 20px 40px 20px 20px;
+  padding: 20px 20px 20px 20px;
 }
 </style>
