a5d722f28211df685191cd146764440a3e3018e2
Jasonccj <981015619@qq.com>
2019-05-06 23:17:43 +0800

    #更新首页内容显示

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -2,51 +2,38 @@
   <div class="container">
     <div class="block">
       <el-timeline>
-        <el-timeline-item timestamp="1.0.2" placement="top" color="#0bbd87">
+        <el-timeline-item timestamp="1.0.3 (2019/4/29)" placement="top" color="#0bbd87">
           <el-card>
-            <h5>新增：</h5>
             <ol>
-              <li>122</li>
-              <li>2312312</li>
-              <li>2312312</li>
+              <li>新增菜单更新中内容</li>
             </ol>
-            <h5>修改：</h5>
+          </el-card>
+        </el-timeline-item> 
+        <el-timeline-item timestamp="1.0.3 (2019/4/27)" placement="top" color="#0bbd87">
+          <el-card>
             <ol>
-              <li>122</li>
-              <li>2312312</li>
-              <li>2312312</li>
+              <li>新增week-css:1.垂直时间轴</li>
+            </ol>
+          </el-card>
+        </el-timeline-item>        
+        <el-timeline-item timestamp="1.0.2 (2019/4/25)" placement="top" color="#0bbd87">
+          <el-card>
+            <ol>
+              <li>新增个人博客，基础组件，常用业务组件，week-css左侧菜单</li>
             </ol>
           </el-card>
         </el-timeline-item>
-        <el-timeline-item timestamp="1.0.1" placement="top" color="#0bbd87">
+        <el-timeline-item timestamp="1.0.1 (2019/4/13)" placement="top" color="#0bbd87">
           <el-card>
-            <h5>新增：</h5>
             <ol>
-              <li>122</li>
-              <li>2312312</li>
-              <li>2312312</li>
-            </ol>
-            <h5>修改：</h5>
-            <ol>
-              <li>122</li>
-              <li>2312312</li>
-              <li>2312312</li>
+              <li>修改框架路由及webpack部分配置</li>
             </ol>
           </el-card>
         </el-timeline-item>
-        <el-timeline-item timestamp="1.0.0" placement="top" color="#0bbd87">
+        <el-timeline-item timestamp="1.0.0 (2019/4/1)" placement="top" color="#0bbd87">
           <el-card>
-            <h5>新增：</h5>
             <ol>
-              <li>122</li>
-              <li>2312312</li>
-              <li>2312312</li>
-            </ol>
-            <h5>修改：</h5>
-            <ol>
-              <li>122</li>
-              <li>2312312</li>
-              <li>2312312</li>
+              <li>网站基础模板搭建</li>
             </ol>
           </el-card>
         </el-timeline-item>
@@ -66,7 +53,7 @@ export default {
   padding: 20px 40px 20px 20px;
   h5 { color: #409EFF };
   ol {
-    list-style: none;
+    list-style: disc;
   }
 }
 </style>
